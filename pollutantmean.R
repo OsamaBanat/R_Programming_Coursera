899c3402c5e7f716834f7314253c9aa307f1a53b
OsamaBanat <o.banat42@gmail.com>
2026-04-30 14:18:32 +0300

    Update pollutantmean.R

--- pollutantmean.R
+++ pollutantmean.R
@@ -1,11 +1,10 @@
-pollutantmean <- function(directory, pollutant, id = 1:332){
-   all_data <- c()
-   for (i in id){
-      file_name <- sprintf("%03d.csv", i)
-      file_path <- file.path (directory, file_name)
-      df <- read.csv(file_path)
-      all_data <- c(all_data, df [[pollutant]])
-   }
-   mean (all_data, na.rm = TRUE)
+pollutantmean2 <- function(directory, pollutant, id = 1:332){
+files <- list.files(directory, full.names = TRUE)
+all_data <- c()
+  	 for (i in id){
+      		data <- read.csv(files[i])
+      		all_data <- c(all_data, data [[pollutant]])
+   	}
+   	mean (all_data, na.rm = TRUE)
 }
 
